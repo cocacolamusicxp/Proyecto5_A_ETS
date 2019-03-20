37920b599b084618f4e682a41ec58ffd1d1af70b
Daniel <Daniel>
2019-03-20 21:19:21 +0000

    2. Comentarios añadidos al código

--- src/proyecto5_a/Proyecto5_A_main.java
+++ src/proyecto5_a/Proyecto5_A_main.java
@@ -1,8 +1,5 @@
 package proyecto5_a;
 
-import static proyecto5_a.Proyecto_5_A_metodos.mejorAlumno;
-import static proyecto5_a.Proyecto_5_A_metodos.mejorNota;
-
 /*Esto es un comentario
 ocupando varias líneas*/
 
@@ -10,10 +7,12 @@ ocupando varias líneas*/
  */
 public class Proyecto5_A_main {
     public static void main(String[] args){
+        Proyecto_5_A_metodos obj = new Proyecto_5_A_metodos();
+        
         double[] notas = {2, 3.6, 8.4, 6.32, 9.75, 1.75, 4.56, 4.9, 6.9, 4.2};
         String[] alumnos = {"Rodrigo del Pueblo", "Roberto Carlos", "Enrique", "Irelia", "Sebas", "Tian", "Ernesto", "César", "Akali", "Snoopy"};
         
-        System.out.println("El índice buscado es: "+mejorNota(notas)+"\n");
-        mejorAlumno(alumnos,mejorNota(notas));//Esto es otro comentario
+        System.out.println("El índice buscado es: "+obj.mejorNota(notas)+"\n");
+        obj.mejorAlumno(alumnos,obj.mejorNota(notas));//Esto es otro comentario
     }
 }
